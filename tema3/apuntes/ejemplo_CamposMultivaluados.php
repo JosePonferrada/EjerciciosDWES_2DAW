0ef583ab7e7ca426519e9f1c433b68fbcb6605e8
JosePonferrada <joseponfe10@gmail.com>
2024-10-23 10:36:43 +0200

    fix(tema3): example CamposMultivaluados added

---
+++ tema3/apuntes/ejemplo_CamposMultivaluados.php
@@ -0,0 +1,71 @@
+<html>
+    <head>
+        <title>title</title>
+    </head>
+    <body>
+        
+        <form action="" method="post">
+            
+            DNI: <input type="text" name="dni"><br>
+            Nombre: <input type="text" name="nombre"><br>
+            Apellidos: <input type="text" name="apell"><br>
+            Salario: <input type="text" name="salario"><br>
+            Usuario: <input type="text" name="user"><br>
+            Password: <input type="password" name="pass"><br>
+            Idiomas: <br>
+            <input type="checkbox" name="idiomas[]" value="1">Español<br>
+            <input type="checkbox" name="idiomas[]" value="2">Inglés<br>
+            <input type="checkbox" name="idiomas[]" value="4">Francés<br>
+            <input type="checkbox" name="idiomas[]" value="8">Alemán<br>
+            <input type="checkbox" name="idiomas[]" value="16">Chino<br>
+            
+            Estudios: 
+            <select name="estudios[]" multiple="">
+            
+                <option value="ESO">ESO</option>
+                <option value="Bachillerato">Bachillerato</option>
+                <option value="CFGM">CFGM</option>
+                <option value="CFGS">CFGS</option>
+                
+            </select><br>
+            
+            <input type="submit" name="guardar" value="Guardar">
+            <input type="submit" name="recuperar" value="Recuperar">
+            
+        </form>
+        
+        <?php
+        
+        if (isset($_POST['guardar'])) {
+            
+            try {
+                
+                $idioma = 0;
+                $conex = new mysqli("localhost", "dwes", "abc123.", "marketing");
+                $conex->set_charset("utf8mb4");
+                
+                // Recorremos el array antes del insert para insertar lo seleccionado
+                // IMPORTANTE inicializar la variable antes porque null + algo dará un error
+                
+                foreach ($_POST['idiomas'] as $value) {
+                    
+                    $idioma += $value;
+                }
+                
+                $arrayEstudios = implode("-", $_POST['estudios']);
+                
+                $conex->query("INSERT INTO empleados values ('$_POST[dni]', '$_POST[nombre]', '$_POST[apell]', "
+                        . "'$_POST[salario]', '$_POST[user]', '$_POST[pass]', $idioma, $arrayEstudios)");
+                
+            } catch (Exception $exc) {
+                die($exc->getMessage());
+            }
+            
+            echo "<br>Registro insertado correctamente<br>";
+            $conex -> close();
+        }
+        
+        ?>
+        
+    </body>
+</html>
