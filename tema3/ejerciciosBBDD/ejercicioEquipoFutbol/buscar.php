9b7c8dc491a366b75076814d3b51bac6485d60d7
JosePonferrada <joseponfe10@gmail.com>
2024-10-28 12:06:36 +0100

    fix(tema3): added search page to BBDD futbol

--- tema3/ejerciciosBBDD/ejercicioEquipoFutbol/buscar.php
+++ tema3/ejerciciosBBDD/ejercicioEquipoFutbol/buscar.php
@@ -1,7 +1,119 @@
-<?php
+<html>
+    <head>
+        <title>Buscar</title>
+    </head>
+    <body>
 
-/* 
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Scripting/EmptyPHP.php to edit this template
- */
+        <h1>Búsqueda de jugadores</h1>
+        
+        <form action="" method="post">
+            Buscar por: 
+            <select name="filterBuscar">
+                <option value="DNI">DNI</option>
+                <option value="team">Equipo</option>
+                <option value="posicion">Posición</option>
+            </select><br><br>
+            
+            Valor a buscar: <input type="text" name="info"><br><br>
+            
+            <input type="submit" name="search" value="Buscar">
+        </form>
+        
+        <?php
+        
+        // Now we gonna validate the data
+        
+        $dni_flag = false; $pos_flag = false; $team_flag = false;
+        
+        if (isset($_POST['search'])) {
+            
+            switch ($_POST['filterBuscar']) {
+                case 'DNI':
+                    if (preg_match('/\d{8}[A-Z]/', $_POST['info'])) {    
+                        $dni_flag = true;
+                    }
+                    break;
+                
+                case 'team':
+                    if (preg_match('/^([a-z]+\s?)+$/i', $_POST['info'])) { 
+                        $team_flag = true;
+                    }
+                    break;
+                
+                case 'posicion':
+                    if (preg_match('/^[a-zA-Z]+$/', $_POST['info'])) {
+                        $pos_flag = true;
+                    }
+                    break;
 
+            }
+            
+        }
+        
+        // Now we have validated the data we must connect to the database and do the query
+        
+        if (isset($_POST['search']) && ($dni_flag || $pos_flag || $team_flag)) {
+            
+            try {
+                $conex = new mysqli("localhost", "dwes", "abc123.", "futbol");
+                $conex->set_charset("utf8mb4");
+
+                $query = ("select * from equipo where ");
+                $searchValue = $_POST['info'];
+
+                if ($dni_flag == true) {
+                    $query .= "DNI = ?";
+                    $searchValue = $_POST['info'];
+                }            
+
+                elseif ($team_flag == true) {
+                    $query .= "Equipo like ?";
+                    $searchValue = "%".$_POST['info']."%";
+                }
+
+                elseif ($pos_flag == true) {
+                    $query .= "Posicion like ?";
+                    $searchValue = "%".$_POST['info']."%";
+                }
+
+                // When we have the query, we can do the preparedStatement with the query
+
+                $stmt = $conex->prepare($query);
+                $stmt ->bind_param('s', $searchValue);
+
+                if ($stmt->execute()) {
+
+                    $result = $stmt->get_result();
+
+                    while ($fila = $result->fetch_object()) {
+
+                        echo "Nombre: ".$fila->Nombre."<br>";
+                        echo "DNI: ".$fila->DNI."<br>";
+                        echo "Dorsal: ".$fila->Dorsal."<br>";
+                        echo "Posición: ".$fila->Posicion."<br>";
+                        echo "Equipo: ".$fila->Equipo."<br>";
+                        echo "Goles: ".$fila->Goles."<br>";
+
+                        echo "<br>==================================<br>";
+
+                    }
+
+                }
+
+
+            } catch (Exception $ex) {
+
+                die("Error al conectar<br>");
+
+            }
+
+            // We close the conex
+
+            $conex->close();
+
+        }
+        
+        ?>
+        
+    </body>
+</html>
