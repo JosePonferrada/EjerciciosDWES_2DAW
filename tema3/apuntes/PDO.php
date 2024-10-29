4644b60ecd5b6bb11ec8a7f7dc36fca88a8eb271
JosePonferrada <joseponfe10@gmail.com>
2024-10-29 09:27:44 +0100

    fix(tema3-apuntes): finished example

--- tema3/apuntes/PDO.php
+++ tema3/apuntes/PDO.php
@@ -32,4 +32,72 @@
     }
 
     
+    // CONSULTA
+    
+    try {
+        // Devuelve un objeto de la clase PDOStatement
+        $result = $conex->query("SELECT * FROM producto");
+        echo "<br>Números de registros devueltos: ".$result->rowCount()."<br>";
+        
+        // Vamos a recorrer los resultados y mostrarlos
+//        while ($fila = $result->fetch()) {
+//            
+//            echo "Nombre: ".$fila['nombre_corto']."<br>";
+//            
+//        }
+//        
+        // Otra forma de recorrer los resultados y mostrarlos
+//        while ($fila = $result->fetch(PDO::FETCH_OBJ)) {
+//            
+//            echo "Nombre: ".$fila->nombre_corto."<br>";
+//            
+//        }
+        
+        // Otra forma distinta de recorrer los resultados y mostrarlos
+        while ($fila = $result->fetchObject()) {
+            
+            echo "Nombre: ".$fila->nombre_corto."<br>";
+            
+        }
+        
+    } catch (PDOException $exc) {
+        echo $exc->getMessage();
+
+    }
+
+    // CONSULTAS PREPARADAS
+    
+    echo "<br>============================================<br>";
+    
+    $menor = 100;
+    $mayor = 200;
+    
+    try {
+                
+//        $result = $conex->prepare("SELECT * FROM producto where PVP between ? and ?");
+        // Se le puede dar un nombre poniendo los 2 puntos delante
+        $result = $conex->prepare("SELECT * FROM producto where PVP between :pvp1 and :pvp2");
+        // De esta manera se asignan los valores si usamos las interrogaciones
+        $result->bindParam(1, $menor);
+        $result->bindParam(2, $mayor);
+        
+        // Así se le asignan los valores si usamos los nombres
+//        $result->bindParam(":pvp1", $menor);
+//        $result->bindParam(":pvp2", $mayor);
+        
+        // Otra opción es pasarle un array con los parámetros
+        $result->execute(array(":pvp1" => $menor, ":pvp2" => $mayor));
+        
+//        $result->execute();
+        echo "Filas devueltas: ".$result->rowCount()."<br>";
+        
+        while ($fila = $result->fetchObject()) {
+            echo "<br>Nombre: ".$fila->nombre_corto."<br>";
+        }
+        
+    } catch (PDOException $exc) {
+        echo $exc->getMessage();
+    }
+
+    
 ?>
