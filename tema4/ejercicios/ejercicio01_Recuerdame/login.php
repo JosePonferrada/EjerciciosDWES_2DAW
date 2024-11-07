9dc64b7bd27ff30f5e1316be1280a2445f8d4ac0
JosePonferrada <joseponfe10@gmail.com>
2024-11-07 13:03:54 +0100

    fix(tema4): login exercise with remember and register started

---
+++ tema4/ejercicios/ejercicio01_Recuerdame/login.php
@@ -0,0 +1,23 @@
+<html>
+    <head>
+        <title>Login</title>
+    </head>
+    <body>
+
+        <form action="" method="post">
+            
+            <p>Usuario: <input type="text" name="user"></p>
+            <p>Clave: <input type="text" name="pass"></p>
+            
+            <p>Recuérdame <input type="checkbox" name="remember"></p>
+            
+            <p>
+                <input type="submit" name="access" value="Acceder">
+                <a href='registro.php'><input type='button' name='register' value='Registrar'></a>
+                
+            </p>
+            
+        </form>
+        
+    </body>
+</html>
---
+++ tema4/ejercicios/ejercicio01_Recuerdame/registro.php
@@ -0,0 +1,24 @@
+<html>
+    <head>
+        <title>Registro</title>
+    </head>
+    <body>
+        
+        <form action="" method="post">
+            
+            <p>DNI: <input type="text" name="DNI"></p>
+            <p>Nombre: <input type="text" name="name"></p>
+            <p>Apellidos: <input type="text" name="surname"></p>
+            <p>Usuario: <input type="text" name="user"></p>
+            <p>Clave: <input type="text" name="pass"></p>
+
+            <p>
+                <a href='login.php'><input type='button' name='login' value='Login'></a>
+                <input type="submit" name="register" value="Registrar">
+
+            </p>
+            
+        </form>
+        
+    </body>
+</html>
