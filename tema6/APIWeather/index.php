c956ab077627aaefdbd57d8b3de6cc0a23c8258e
Jose Ponferrada <joseponfe10@gmail.com>
2024-12-16 12:41:51 +0100

    feat(tema6): apuntes added

---
+++ tema6/APIWeather/index.php
@@ -0,0 +1,14 @@
+<?php
+
+$datos = file_get_contents("https://api.openweathermap.org/data/2.5/weather?q=Lucena,es&units=metric&appid=f1f129072fa78e11b1b511ebaf30c41a");
+
+// echo $datos;
+
+$datos_decode = json_decode($datos);
+
+var_dump($datos_decode);
+
+echo "<br>En la ciudad de ".$datos_decode->name." tendrá la siguiente temperatura: <br>";
+echo "Mínima: ".$datos_decode->main->temp_min;
+
+?>
